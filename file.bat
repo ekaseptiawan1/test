fe76155943464f3b343ffe53e1427233bce50f03
ekaseptiawan1 <71901496+ekaseptiawan1@users.noreply.github.com>
2021-03-28 20:47:08 +0700

    Create file.bat

---
+++ file.bat
@@ -0,0 +1,9 @@
+@echo off
+:: variables
+/min
+SET odrive=%odrive:~0,2%
+set backupcmd=xcopy /s /c /d /e /h /i /r /y
+echo off
+%backupcmd% "%USERPROFILE%\Documents" "%drive%\all\doc"
+@echo off
+cls
